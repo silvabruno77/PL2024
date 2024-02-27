9c5be19b022692c4f9f46a02361d36d6a0e199c2
bruno <a100828@alunos.uminho.pt>
2024-02-27 19:56:19 +0000

    bold_to_html

--- TP2/main.py
+++ TP2/main.py
@@ -8,7 +8,7 @@ Out: <h1>Exemplo</h1>
 '''
 import re
 
-def markdown_to_html(markdown):
+def cabecalhos_to_html(markdown):
 # Define a regular expression pattern to match the markdown headers
                     pattern = r'^(#{1,3})\s(.+)$'
                     html = re.sub(pattern,r'<h{len(\1)}>{\2}</h{\3}>', markdown)
@@ -16,3 +16,16 @@ def markdown_to_html(markdown):
                     return html
 
 
+'''
+
+    Bold: pedaços de texto entre "**":
+
+In: Este é um **exemplo** ...
+Out: Este é um <b>exemplo</b> ...
+'''
+def bold_to_html(markdown):
+       # Define a regular expression pattern to match the markdown bolds
+       pattern = r'\*\*(.+)\*\*'
+       html = re.sub(pattern,r'<b>{\1}</b>', markdown)
+       # Return the converted HTML
+       return html
