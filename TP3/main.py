4a4c02419dfc9a2fae89e5a05c39d343d19466a6
bruno <a100828@alunos.uminho.pt>
2024-03-04 14:17:53 +0000

    TP3

--- TP3/main.py
+++ TP3/main.py
@@ -1,31 +1,54 @@
 '''
-Criar em Python um pequeno conversor de MarkDown para HTML para os elementos descritos na "Basic Syntax" da Cheat Sheet:
 
-    Cabeçalhos: linhas iniciadas por "# texto", ou "## texto" ou "### texto"
+Somador on/off
+Somador on/off: criar o programa em Python
 
-In: # Exemplo
-Out: <h1>Exemplo</h1>
+    Pretende-se um programa que some todas as sequências de dígitos que encontre num texto;
+    Sempre que encontrar a string “Off” em qualquer combinação de maiúsculas e minúsculas, esse comportamento é desligado;
+    Sempre que encontrar a string “On” em qualquer combinação de maiúsculas e minúsculas, esse comportamento é novamente ligado;
+    Sempre que encontrar o caráter “=”, o resultado da soma é colocado na saída.
+
 '''
+def somador_on_off(texto):
+    somar = False
+    resultado = 0
+    for c in texto:
+        if c.lower() == 'o':
+            somar = False
+        elif c.lower() == 'n':
+            somar = True
+        elif c.isdigit() and somar:
+            resultado += int(c)
+        elif c == '=':
+            return resultado
+    return resultado
+
+
+
+
+"""
+Com expreções regulares o somador_on_off pode ser escrito da seguinte forma:
+Foi dado um exemplo na aula, usei como base
+"""
 import re
+def somador_on_off_re(texto):
+    texto = ''' 
+    Este é apenas 1 texto demonstrativo.
+    Amanhã, 2023-03-07, tens de ir visitar o teu amigo.
+    Até agora totalizamos: =
+    ofF
+    Agora já não está a somar os números: 1, 2, ...
+    On, ligou-se outra vez, cuidado com o 1, 2, 3 ...
+    Chegamos ao fim com =
+    '''
 
-def cabecalhos_to_html(markdown):
-# Define a regular expression pattern to match the markdown headers
-                    pattern = r'^(#{1,3})\s(.+)$'
-                    html = re.sub(pattern,r'<h{len(\1)}>{\2}</h{\3}>', markdown)
-                    # Return the converted HTML
-                    return html
+    token_specification = [
+            ('INT', r'(\+|-)?\d+'),             
+            ('ON',  r'(?i:on)'),             
+            ('OFF', r'(?i:off)'),
+            ('EQ', r'='),
+            ('SKIP', r'\s+'),
+            ('UNKNOWN', r'.')
+        ]
 
-
-'''
-
-    Bold: pedaços de texto entre "**":
-
-In: Este é um **exemplo** ...
-Out: Este é um <b>exemplo</b> ...
-'''
-def bold_to_html(markdown):
-       # Define a regular expression pattern to match the markdown bolds
-       pattern = r'\*\*(.+)\*\*'
-       html = re.sub(pattern,r'<b>{\1}</b>', markdown)
-       # Return the converted HTML
-       return html
+    mypattern = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
